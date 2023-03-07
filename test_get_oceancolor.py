b1765ab84ba285d9f84d74fdad90c46bdaba5666
Ana Krelling <ana@axiomdatascience.com>
2023-03-07 16:45:45 -0500

    fix funcs + tmpdir

--- test_get_oceancolor.py
+++ test_get_oceancolor.py
@@ -23,31 +23,38 @@ def settings_dict():
     }
 
 
+# temporary folder to store filelist.txt
+@pytest.fixture(scope="function")
+def tmpdir(tmp_path):
+    tmp_dir = tmp_path
+    return tmp_dir
+
+
+# see if url to search for filenames is valid
 def test_valid_file_search_url():
     file_search_url = "https://oceandata.sci.gsfc.nasa.gov/api/file_search"
     url_code = urlopen(file_search_url).getcode()
     assert url_code == 200
 
 
-def test_valid_get_filelist_command(settings_dict):
-    curl_command = get_filelist_command(settings_dict)
+# get individual file names
+def test_valid_get_filelist_command(settings_dict, tmpdir):
+    curl_command = get_filelist_command(settings_dict, datadir=tmpdir)
     output = os.popen(curl_command).read()
     assert output != "No Results Found\n"
 
 
+# check base url to go fetch the data
 def test_valid_opendap_base_url(settings_dict):
     url = settings_dict["opendap_base_url"]
     url_code = urlopen(url).getcode()
     assert url_code == 200
 
 
-def test_get_opendap_urls(settings_dict):
-    dataset_urls = get_opendap_urls(settings_dict)
+# urls to go fetch individual data file
+def test_get_opendap_urls(settings_dict, tmpdir):
+    dataset_urls = get_opendap_urls(settings_dict, datadir=tmpdir)
     assert len(dataset_urls) == 2
 
 
-@pytest.mark.skip("no way of currently testing this")
-def test_any_files_in_filelist():
-    with open("data/filelist.txt", mode="r") as f:
-        file_list = list(f)
-    assert len(file_list) > 0
+# it would be nice to get new tests to check if the number of results is correct
